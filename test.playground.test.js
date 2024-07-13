4aab691f08c850b678bb32ddd3c9fa75e0d861cb
ExtF8 <extfield8@gmail.com>
2024-07-13 08:19:55 +0300

    added tests for first three assignments

--- test.playground.test.js
+++ test.playground.test.js
@@ -1,5 +1,23 @@
-import sum from './test.playground';
+import { sum, capitalize, reverseString, calculatorObject } from './test.playground';
+
 
 test('adds 1 + 2 to equal 3', () => {
     expect(sum(1, 2)).toBe(3);
 });
+
+test('the first character capitalized', () => {
+    expect(capitalize('text')).toBe('Text')
+});
+
+test('reversed string', () => {
+    expect(reverseString('text')).toMatch('txet')
+})
+
+describe('calculator Object', () => {
+    test('calculator methods', () => {
+        expect(calculatorObject.add(1, 2)).toBe(3);
+        expect(calculatorObject.subtract(4, 2)).toBe(2);
+        expect(calculatorObject.divide(12, 3)).toBe(4);
+        expect(calculatorObject.multiply(4, 4)).toBe(16)
+    })
+})
